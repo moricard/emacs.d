294fa7fa6cbb752f14bd7b271c024bf42adb1063
marcus <marc-olivierricard@whitelion.local>
2012-12-09 17:12:28 -0500

    Adds yasnippets

---
+++ default-behavior.el
@@ -0,0 +1,52 @@
+;; Default behavior
+
+;; Command key is meta
+
+
+;; Allow pasting selection outside of Emacs
+(setq x-select-enable-clipboard t)
+
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
+
+
+;; Answering just 'y' or 'n' will do
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; UTF-8 please
+(setq locale-coding-system 'utf-8) ; pretty
+(set-terminal-coding-system 'utf-8) ; pretty
+(set-keyboard-coding-system 'utf-8) ; pretty
+(set-selection-coding-system 'utf-8) ; please
+(prefer-coding-system 'utf-8) ; with sugar on top
+
+;; Show active region
+(transient-mark-mode 1)
+(make-variable-buffer-local 'transient-mark-mode)
+(put 'transient-mark-mode 'permanent-local t)
+(setq-default transient-mark-mode t)
+
+;; Remove text in active region if inserting text
+(delete-selection-mode 1)
+
+;; Always display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; Lines should be 80 characters wide, not 72
+(setq fill-column 80)
+
+(provide 'default-behavior)
--- init.el
+++ init.el
@@ -1,4 +1,50 @@
+;; Set path to .emacs.d
+(setq emacs-dir (file-name-directory
+		 (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path emacs-dir)
 
+
+;;Appearance
 ;; Solarized colors
-(add-to-list 'custom-theme-load-path "~/.emacs.d/solarized-colors")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/modules/solarized-colors")
 (load-theme 'solarized-dark t)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
+
+;; No menu bars
+(menu-bar-mode -1)
+
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b"))))
+
+;; Ditch them scrollbars
+(scroll-bar-mode -1)
+
+
+
+
+;; Default behavior
+(require 'default-behavior) 
+
+
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
