5b29c8b018b35ab4f3ec57e2f68365fd742b3ce3
Marc-Olivier Ricard <molivier@keatext.com>
2013-02-12 08:41:10 -0500

    move backup files to temp dir and add a few snippets

--- default-behavior.el
+++ default-behavior.el
@@ -1,5 +1,23 @@
 ;; Default behavior
 
+;; Move backups to system temp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Cleanup temp directory from files not accessed within a week
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
+
+
 ;; Allow pasting selection outside of Emacs
 (setq x-select-enable-clipboard t)
 
